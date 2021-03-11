e0f90ab6fbbcfb584d1ce63f66b1cb4393df0d78
ezhopets <ezhopets@gmail.com>
2021-03-11 15:45:48 +0300

    Added number buttons

--- 03_ThreeWayAndTkinter/main.py
+++ 03_ThreeWayAndTkinter/main.py
@@ -14,11 +14,31 @@ class Application(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
+        for i in range(1, 4):
+            self.rowconfigure(i, weight=1)
+            self.columnconfigure(i, weight=1)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(4, weight=1)
+
         self.new = tk.Button(self, text='New')
         self.quit = tk.Button(self, text='Quit', command=self.quit)
 
-        self.new.grid(row=0, column=0, sticky="NS")
-        self.quit.grid(row=0, column= 1, sticky="NS")
+        self.buttons = dict()
+        for i in range(1, 16):
+            self.buttons[i] = tk.Button(self, text = str(i))
+
+        row_ = 1
+        col_ = 0
+        for i in range(1, 16):
+            self.buttons[i].grid(row = row_, column = col_, sticky = "NEWS")
+            col_ += 1
+            if col_ == 4:
+                col_ = 0
+                row_ += 1
+
+        self.new.grid(row=0, column=0, columnspan = 2, sticky="NS")
+        self.quit.grid(row=0, column= 2, columnspan = 2, sticky="NS")
 
 
 
