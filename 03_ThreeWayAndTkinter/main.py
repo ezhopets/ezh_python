14532e5802c36d4fbb45eca17c3e247fed439f61
ezhopets <ezhopets@gmail.com>
2021-03-12 12:41:04 +0300

    Added movement to number buttons

--- 03_ThreeWayAndTkinter/main.py
+++ 03_ThreeWayAndTkinter/main.py
@@ -14,6 +14,9 @@ class Application(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
+        self.void_col = 3
+        self.void_row = 4
+
         for i in range(1, 4):
             self.rowconfigure(i, weight=1)
             self.columnconfigure(i, weight=1)
@@ -26,7 +29,7 @@ class Application(tk.Frame):
 
         self.buttons = dict()
         for i in range(1, 16):
-            self.buttons[i] = tk.Button(self, text = str(i))
+            self.buttons[i] = tk.Button(self, text = str(i), command=lambda i=i: self.change_location(i))
 
         row_ = 1
         col_ = 0
@@ -40,6 +43,13 @@ class Application(tk.Frame):
         self.new.grid(row=0, column=0, columnspan = 2, sticky="NS")
         self.quit.grid(row=0, column= 2, columnspan = 2, sticky="NS")
 
+    def change_location(self, i):
+        row = self.buttons[i].grid_info()['row']
+        col = self.buttons[i].grid_info()['column']
+        if col == self.void_col and abs(row - self.void_row) == 1 or row == self.void_row and abs(col - self.void_col) == 1:
+            self.buttons[i].grid(row = self.void_row, column = self.void_col)
+            self.void_col = col
+            self.void_row = row
 
 
 def main():
