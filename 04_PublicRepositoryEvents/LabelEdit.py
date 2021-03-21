08c4baa0506d1c0876b0cdf810441e963dd6db06
ezhopets <ezhopets@gmail.com>
2021-03-21 22:57:58 +0300

    Add a simple calculator

--- 04_PublicRepositoryEvents/LabelEdit.py
+++ 04_PublicRepositoryEvents/LabelEdit.py
@@ -3,7 +3,6 @@ import tkinter.font as tkFont
 
 
 class Application(tk.Frame):
-
     def __init__(self, master=None, title = 'App', **kwargs):
         super().__init__(master, **kwargs)
 
@@ -14,12 +13,12 @@ class Application(tk.Frame):
         self.grid(sticky="NEWS")
         self.configure(background='DarkOliveGreen1')
 
-
         self.buttons_design = dict(activebackground = 'DarkOliveGreen3',\
                 activeforeground= 'grey10', bd = 4, bg = 'DarkOliveGreen2',\
                 fg = 'grey20')
 
         self.createWidgets()
+
 
     def createWidgets(self, *kwargs):
         self.rowconfigure(0, weight = 1)
@@ -29,16 +28,13 @@ class Application(tk.Frame):
         textvar = tk.StringVar()
         textvar.set("pupa")
 
-
         self.quit = tk.Button(self, font = ('fixed', 10,  'bold'),text='Quit', command=self.master.quit, **self.buttons_design)
-        self.l1 = InputLabel(self, font= "fixed", text='lupa', cursor='heart', takefocus=1, highlightthickness = 3, anchor = 'w')
-        self.l2 = InputLabel(self, font= "fixed", textvariable=textvar, cursor='spider', takefocus=1, highlightthickness = 3,  anchor='w')
-        self.l3 = InputLabel(self, font= "fixed", cursor='star', takefocus=1, highlightthickness = 3, anchor='w')
+        self.l = InputLabel(self, font= "fixed", text='lupa', cursor='heart', takefocus=1, highlightthickness = 3, anchor = 'w')
+        self.calc = tk.Button(self, font = ('fixed', 10,  'bold'),text=' = ', command = self.l.calc, **self.buttons_design)
 
-        self.l1.grid(row=0, column=0, sticky = "EW")
-        self.l2.grid(row=1, column=0, sticky = "EW")
-        self.l3.grid(row=2, column=0, sticky = "EW")
-        self.quit.grid(row=3, column=0,sticky = "ES")
+        self.l.grid(row=0, column=0, columnspan = 2, sticky = "EW")
+        self.calc.grid(row=1, column=0,sticky = "WS")
+        self.quit.grid(row=1, column=1,sticky = "ES")
 
 
 class InputLabel(tk.Label):
@@ -80,31 +76,58 @@ class InputLabel(tk.Label):
 
         self.createWidgets()
 
+
     def createWidgets(self):
         self.xVar = tk.IntVar()
+        self.xVar.set(0)
         self.y = 0
 
-        self.xVar.set(0)
         self.frame = tk.Frame(self, height= self.metrix['linespace'], width = 4, bg = 'red')
+
 
     def key_press(self, event):
         if (event.char):
             if self.textvar:
                 self.textvar.set(self.textvar.get()[:self.cur_pos] + event.char + self.textvar.get()[self.cur_pos:])
-
-                self.xVar.set(self.xVar.get() + self.m_len)
-                self.frame.place(x= self.xVar.get(), y = self.y)
-                self.right_border += self.m_len
-                self.cur_pos += 1
             else:
                 self.config(text = self["text"][:self.cur_pos] + event.char + self["text"][self.cur_pos:])
-                self.xVar.set(self.xVar.get() + self.m_len)
-                self.frame.place(x= self.xVar.get(), y = self.y)
-                self.right_border += self.m_len
-                self.cur_pos += 1
+
+            self.xVar.set(self.xVar.get() + self.m_len)
+            self.frame.place(x= self.xVar.get(), y = self.y)
+            self.right_border += self.m_len
+            self.cur_pos += 1
+
 
     def ignore(self, event):
         pass
+
+
+    def calc(self, *args, **kwargs):
+        def sequre_check(s):
+            for c in s:
+                if c.isalpha():
+                    return False
+
+            return True
+
+        if self.textvar:
+            if sequre_check(self.textvar.get()):
+                self.textvar.set(str(eval(self.textvar.get())))
+            else:
+                self.textvar.set("Error")
+
+        else:
+            if sequre_check(self["text"]):
+                self.config(text = str(eval(self["text"])))
+            else:
+                self.config(text = "Error")
+
+        self.right_border = len(self['text']) * self.m_len
+
+        self.xVar.set(self.right_border)
+        self.frame.place(x= self.xVar.get(), y = self.y)
+        self.cur_pos = self.right_border // self.m_len
+
 
     def move_left(self, event = None):
         new_x = self.xVar.get() - self.m_len
@@ -124,17 +147,20 @@ class InputLabel(tk.Label):
             self.frame.place(x= self.xVar.get(), y = self.y)
             self.cur_pos += 1
 
+
     def move_home(self, event):
         new_x = 0
         self.xVar.set(new_x)
         self.frame.place(x= self.xVar.get(), y = self.y)
         self.cur_pos = 0
 
+
     def move_end(self, event):
         new_x = self.right_border
         self.xVar.set(new_x)
         self.frame.place(x= self.xVar.get(), y = self.y)
         self.cur_pos = self.right_border // self.m_len
+
 
     def back_space(self, *arg):
         if self.textvar and self.textvar.get():
@@ -146,40 +172,42 @@ class InputLabel(tk.Label):
                 self.config(text = self["text"][:self.cur_pos] + self["text"][self.cur_pos + 1:])
                 self.right_border -= self.m_len
 
+
     def delete(self, *arg):
         if self.textvar and self.textvar.get():
             self.textvar.set(self.textvar.get()[:self.cur_pos] + self.textvar.get()[self.cur_pos + 1:])
-            new_x = self.xVar.get() + self.m_len
-            if (new_x <= self.right_border):
-                self.right_border -= self.m_len
         elif self["text"]:
             self.config(text = self["text"][:self.cur_pos] + self["text"][self.cur_pos + 1:])
-            new_x = self.xVar.get() + self.m_len
-            if (new_x <= self.right_border):
-                self.right_border -= self.m_len
+
+        new_x = self.xVar.get() + self.m_len
+        if (new_x <= self.right_border):
+            self.right_border -= self.m_len
 
 
     def focus_in(self, event):
         self.frame.place(x= self.xVar.get(), y = self.y)
 
+
     def focus_out(self, event):
         self.frame.place_forget()
 
+
     def foc(self, event):
         self.focus_force()
+
         if (event.x > self.right_border):
             self.xVar.set(self.right_border)
-            self.cur_pos = self.right_border // self.m_len
-            self.frame.place(x= self.xVar.get(), y = self.y)
         else:
             self.xVar.set((event.x + (1/4) * self.m_len) // self.m_len * self.m_len)
-            self.cur_pos = self.xVar.get() // self.m_len
-            self.frame.place(x= self.xVar.get(), y = self.y)
+
+        self.cur_pos = self.xVar.get() // self.m_len
+        self.frame.place(x= self.xVar.get(), y = self.y)
 
 
 def main():
     app = Application()
     app.mainloop()
+
 
 if __name__ == "__main__":
     main()
